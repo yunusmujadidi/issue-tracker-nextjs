0408f41d2dfa856fb159ea0f537de0786820e697
Ahmad Yunus <yunusmujadidi@gmail.com>
2023-11-20 20:56:38 +0700

    put logic in navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,6 +1,9 @@
+"use client";
 import { link } from "fs";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { FaBug } from "react-icons/fa";
+import classNames from "classnames";
 
 const NavBar = () => {
   const links = [
@@ -14,6 +17,7 @@ const NavBar = () => {
     },
   ];
 
+  const currentPath = usePathname();
   return (
     <nav className="flex px-5 space-x-5 h-14 items-center border-b mb-5">
       <Link href="/">
@@ -22,7 +26,11 @@ const NavBar = () => {
       <ul className="flex space-x-5">
         {links.map((link) => (
           <Link
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={classNames({
+              "text-zinc-900": currentPath === link.href,
+              "text-zinc-500": currentPath !== link.href,
+              "hover:text-zinc-800": true,
+            })}
             href={link.href}
             key={link.href}
           >
