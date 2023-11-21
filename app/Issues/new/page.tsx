cef8e2650ba7dc4f0e387c62430d4589a9f591c1
Ahmad Yunus <yunusmujadidi@gmail.com>
2023-11-21 18:17:16 +0700

    make an ErrorMessage components

--- app/Issues/new/page.tsx
+++ app/Issues/new/page.tsx
@@ -10,6 +10,7 @@ import { IoIosAlert } from "react-icons/io";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchema";
 import { z } from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -52,11 +53,7 @@ const NewIssuePage = () => {
             {...register("title")}
           ></TextField.Input>
         </TextField.Root>
-        {errors.title && (
-          <Text as="p" color="red">
-            {errors.title.message}
-          </Text>
-        )}
+        {errors.title && <ErrorMessage>{errors.title.message}</ErrorMessage>}
         <Controller
           name="description"
           control={control}
@@ -65,9 +62,7 @@ const NewIssuePage = () => {
           )}
         />
         {errors.description && (
-          <Text color="red" as="p">
-            {errors.description.message}
-          </Text>
+          <ErrorMessage>{errors.description.message}</ErrorMessage>
         )}
 
         <Button>Submit New Issue</Button>
