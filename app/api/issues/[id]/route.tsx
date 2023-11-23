8c17cd30e35701b5e8df44ef576a67a2df384ac9
Ahmad Yunus <yunusmujadidi@gmail.com>
2023-11-23 13:52:46 +0700

    refactor components

---
+++ app/api/issues/[id]/route.tsx
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from "next/server";
+import { IssueSchema } from "@/app/validationSchema";
+import prisma from "@/prisma/client";
+
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+export async function PATCH(request: NextRequest, { params }: Props) {
+  const body = await request.json();
+  const validation = IssueSchema.safeParse(body);
+  if (!validation.success) {
+    return NextResponse.json(validation.error.format(), { status: 400 });
+  }
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
+
+  if (!issue) {
+    return NextResponse.json({ error: "invalid issue" }, { status: 404 });
+  }
+  const updateIssue = await prisma.issue.update({
+    where: {
+      id: issue.id,
+    },
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+  return NextResponse.json(updateIssue, { status: 200 });
+}
