031dc3f2de0135e1b527bee09191f1fb2bc1d327
Ahmad Yunus <yunusmujadidi@gmail.com>
2023-11-23 17:56:40 +0700

    show dialog

--- app/Issues/[id]/DeleteIssueButton.tsx
+++ app/Issues/[id]/DeleteIssueButton.tsx
@@ -1,13 +1,31 @@
-import { Box, Button } from "@radix-ui/themes";
+"use client";
+import { AlertDialog, Box, Button } from "@radix-ui/themes";
 import Link from "next/link";
 import React from "react";
-import { PiNotePencil } from "react-icons/pi";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   return (
-    <Button color="red">
-      <Link href={`/issues/${issueId}/edit`}>Delete Issue</Link>
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">Delete Issue</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Are you sure?</AlertDialog.Title>
+        <AlertDialog.Description>
+          This action is permanent. You can`t undo this action.
+        </AlertDialog.Description>
+        <Box className="flex justify-end space-x-2 mt-4">
+          <AlertDialog.Cancel>
+            <Button>Cancel</Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color="red">
+              <Link href={`/issues/${issueId}/delete`}>Delete Issue</Link>
+            </Button>
+          </AlertDialog.Action>
+        </Box>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 
