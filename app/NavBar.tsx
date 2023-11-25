44ac4c5cf1dde7b25ae0daf17c975df557346f8b
Ahmad Yunus <yunusmujadidi@gmail.com>
2023-11-25 21:35:29 +0700

    add avatar and dropdown

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,7 +4,15 @@ import { usePathname } from "next/navigation";
 import { FaBug } from "react-icons/fa";
 import classNames from "classnames";
 import { useSession } from "next-auth/react";
-import { Box } from "@radix-ui/themes";
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  DropdownMenuLabel,
+  Flex,
+  Text,
+} from "@radix-ui/themes";
 
 const NavBar = () => {
   const { status, data: session } = useSession();
@@ -21,34 +29,58 @@ const NavBar = () => {
 
   const currentPath = usePathname();
   return (
-    <nav className="flex px-5 space-x-5 h-14 items-center border-b mb-5">
-      <Link href="/">
-        <FaBug size={24} />
-      </Link>
-      <ul className="flex space-x-5">
-        {links.map((link) => (
-          <li key={link.href}>
-            <Link
-              className={classNames({
-                "text-zinc-900": currentPath === link.href,
-                "text-zinc-500": currentPath !== link.href,
-                "hover:text-zinc-800": true,
-              })}
-              href={link.href}
-            >
-              {link.label}
+    <Container>
+      <nav className="px-5 space-x-5 border-b mb-5 py-3">
+        <Flex justify="between">
+          <Flex align="center" gap="3">
+            <Link href="/">
+              <FaBug size={24} />
             </Link>
-          </li>
-        ))}
-      </ul>
-      <Box>
-        {status === "authenticated" ? (
-          <Link href="/api/auth/signout">Sign out</Link>
-        ) : (
-          <Link href="/api/auth/signin">Sign in</Link>
-        )}
-      </Box>
-    </nav>
+            <ul className="flex space-x-5">
+              {links.map((link) => (
+                <li key={link.href}>
+                  <Link
+                    className={classNames({
+                      "text-zinc-900": currentPath === link.href,
+                      "text-zinc-500": currentPath !== link.href,
+                      "hover:text-zinc-800": true,
+                    })}
+                    href={link.href}
+                  >
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          <Box>
+            {status === "authenticated" ? (
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                  <Avatar
+                    src={session.user!.image!}
+                    fallback="?"
+                    radius="full"
+                    size="2"
+                    className="cursor-pointer"
+                  />
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content align="end">
+                  <DropdownMenuLabel>
+                    <Text size="2"> {session.user!.email}</Text>
+                  </DropdownMenuLabel>
+                  <DropdownMenu.Item>
+                    <Link href="/api/auth/signout">Sign out</Link>
+                  </DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
+            ) : (
+              <Link href="/api/auth/signin">Sign in</Link>
+            )}
+          </Box>
+        </Flex>
+      </nav>
+    </Container>
   );
 };
 
