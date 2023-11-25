ab0ab6d19d83a88fadaea5377a3cc85f19102b77
Ahmad Yunus <yunusmujadidi@gmail.com>
2023-11-25 20:50:35 +0700

    add login and logout

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,11 +1,13 @@
 "use client";
-import { link } from "fs";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { FaBug } from "react-icons/fa";
 import classNames from "classnames";
+import { useSession } from "next-auth/react";
+import { Box } from "@radix-ui/themes";
 
 const NavBar = () => {
+  const { status, data: session } = useSession();
   const links = [
     {
       href: "/",
@@ -25,19 +27,27 @@ const NavBar = () => {
       </Link>
       <ul className="flex space-x-5">
         {links.map((link) => (
-          <Link
-            className={classNames({
-              "text-zinc-900": currentPath === link.href,
-              "text-zinc-500": currentPath !== link.href,
-              "hover:text-zinc-800": true,
-            })}
-            href={link.href}
-            key={link.href}
-          >
-            {link.label}
-          </Link>
+          <li key={link.href}>
+            <Link
+              className={classNames({
+                "text-zinc-900": currentPath === link.href,
+                "text-zinc-500": currentPath !== link.href,
+                "hover:text-zinc-800": true,
+              })}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
         ))}
       </ul>
+      <Box>
+        {status === "authenticated" ? (
+          <Link href="/api/auth/signout">Sign out</Link>
+        ) : (
+          <Link href="/api/auth/signin">Sign in</Link>
+        )}
+      </Box>
     </nav>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "./NavBar";
 import { Container, Theme } from "@radix-ui/themes";
+import AuthProvider from "./auth/Provider";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -25,12 +26,14 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning={true}>
       <body className={inter.variable}>
-        <Theme appearance="light" accentColor="blue" radius="large">
-          <NavBar />
-          <main>
-            <Container>{children}</Container>
-          </main>
-        </Theme>
+        <AuthProvider>
+          <Theme appearance="light" accentColor="blue" radius="large">
+            <NavBar />
+            <main>
+              <Container>{children}</Container>
+            </main>
+          </Theme>
+        </AuthProvider>
       </body>
     </html>
   );
